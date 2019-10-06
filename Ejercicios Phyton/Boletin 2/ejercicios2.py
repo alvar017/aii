d21d2d55c299a907e87fe8c8ed62b0cb96ec4507
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-10-06 21:37:08 +0200

    Primera version

--- Ejercicios Phyton/Boletin 2/ejercicios2.py
+++ Ejercicios Phyton/Boletin 2/ejercicios2.py
@@ -1,6 +1,19 @@
+import urllib
+from urllib import request
+from urllib.error import HTTPError
 
 
-
+if __name__ == "__main__":
+    try:
+        f = request.urlopen('http://www.us.es/rss/feed/portada')
+        print(f.read())
+        f.close()
+    except HTTPError as e:
+        print("Ocurrió un error")
+        print(e.code)
+    except HTTPError as e:
+        print('Ocurrió un error')
+        print(e.reason)
 
 
 
