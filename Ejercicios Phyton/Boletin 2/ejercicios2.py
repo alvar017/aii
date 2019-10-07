2fb1b1ccec8bb82df1f6b4ca7154a2a8781287c6
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-10-07 17:14:07 +0200

    Ejercicio 2 completo

--- Ejercicios Phyton/Boletin 2/ejercicios2.py
+++ Ejercicios Phyton/Boletin 2/ejercicios2.py
@@ -5,22 +5,29 @@ from urllib.error import HTTPError
 import dateutil.parser
 
 
+def capturar(url):
+    f = request.urlopen(url)
+    captura = f.read().decode(f.headers.get_content_charset())
+    f.close()
+    return captura
+
+
+def filtra(filtro, captura):
+    return re.findall(filtro, captura)
+
+
+def imprime(captura_filtrada):
+    for elemento in captura_filtrada:
+        print('Título: ', elemento[0])
+        print('Link: ', elemento[1])
+        print('Fecha: ', dateutil.parser.parse(elemento[3]).strftime("%d/%m/%Y"), '\n')
+
+
 if __name__ == "__main__":
     try:
-        f = request.urlopen('http://www.us.es/rss/feed/portada')
-        tag = f.read().decode(f.headers.get_content_charset())
-        titulos = re.findall('<title>([^<]*)</title>', tag)
-        links = re.findall('<link>([^<]*)</link>', tag)
-        fechas = re.findall('<pubDate>([^<]*)</pubDate>', tag)
-
-        i = 1
-        while i < len(titulos):
-            print('Título: ', titulos[i])
-            print('Link: ', links[i])
-            print('Fecha: ', dateutil.parser.parse(fechas[i]).strftime("%d/%m/%Y"), '\n')
-            i = i + 1
-
-        f.close()
+        tag = capturar('http://www.us.es/rss/feed/portada')
+        captura_filtrada = filtra(r'<item>\s*<title>(.*)</title>\s*<link>(.*)</link>\s*<description>.*</description>\s*<author>.*</author>\s*(<category>.*</category>)?\s*<guid.*</guid>\s*<pubDate>(.*)</pubDate>\s*</item>', tag)
+        imprime(captura_filtrada)
     except HTTPError as e:
         print("Ocurrió un error")
         print(e.code)
