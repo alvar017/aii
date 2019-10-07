e29bf60e27b41dbc953388863e287b3d9e4c920a
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-10-07 16:51:17 +0200

    Ejercicio 2 casi completo

--- Ejercicios Phyton/Boletin 2/ejercicios2.py
+++ Ejercicios Phyton/Boletin 2/ejercicios2.py
@@ -1,12 +1,25 @@
 import urllib
 from urllib import request
+from urllib.request import re
 from urllib.error import HTTPError
+import dateutil.parser
 
 
 if __name__ == "__main__":
     try:
         f = request.urlopen('http://www.us.es/rss/feed/portada')
-        print(f.read())
+        tag = f.read().decode(f.headers.get_content_charset())
+        titulos = re.findall('<title>([^<]*)</title>', tag)
+        links = re.findall('<link>([^<]*)</link>', tag)
+        fechas = re.findall('<pubDate>([^<]*)</pubDate>', tag)
+
+        i = 1
+        while i < len(titulos):
+            print('Título: ', titulos[i])
+            print('Link: ', links[i])
+            print('Fecha: ', dateutil.parser.parse(fechas[i]).strftime("%d/%m/%Y"), '\n')
+            i = i + 1
+
         f.close()
     except HTTPError as e:
         print("Ocurrió un error")
