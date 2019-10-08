24a14d8c69d1c55a0093d0cbbb699a65e10afa20
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-10-08 12:12:29 +0200

    Primera version de almacenar practica

--- Ejercicios Phyton/Boletin 3/ejercicios3.py
+++ Ejercicios Phyton/Boletin 3/ejercicios3.py
@@ -3,7 +3,8 @@ from tkinter import *
 from urllib import request
 from urllib.request import re
 import dateutil.parser
-from tkinter import messagebox
+import sqlite3 as lite
+import sys
 
 
 class Busca:
@@ -17,55 +18,75 @@ class Busca:
         return re.findall(filtro, captura)
 
     def imprime(self, captura_filtrada):
-        a = []
+        res = []
         for elemento in captura_filtrada:
-            s = ''
-            s = s + 'Título: ' + elemento[0] + "\n"
-            s = s + 'Link: ' + elemento[1] + "\n"
-            s = s + 'Fecha: ' + dateutil.parser.parse(elemento[3]).strftime("%d/%m/%Y") + '\n' + '\n'
-            a.append(s)
-        return a
+            aux = ['Título: ' + elemento[0] + '\n', 'Link: ' + elemento[1] + '\n',
+                   'Fecha: ' + dateutil.parser.parse(elemento[3]).strftime("%d/%m/%Y") + '\n', '\n']
+            res.append(aux)
+        return res
+
+    def imprime_url_filtro(self, url, filtro):
+        busca = Busca()
+        captura = busca.capturar(url)
+        captura_filtrada = busca.filtra(filtro, captura)
+        captura_impresa = busca.imprime(captura_filtrada)
+        return captura_impresa
 
 
 class Ventana:
-
     def __init__(self):
-        top = tk.Tk()
-
         def list():
             busca = Busca()
-            captura = busca.capturar('http://www.us.es/rss/feed/portada')
-            captura_filtrada = busca.filtra(r'<item>\s*<title>(.*)</title>\s*<link>(.*)</link>\s*<description>.*</description>\s*<author>.*</author>\s*(<category>.*</category>)?\s*<guid.*</guid>\s*<pubDate>(.*)</pubDate>\s*</item>', captura)
+            captura_impresa = busca.imprime_url_filtro('http://www.us.es/rss/feed/portada', r'<item>\s*<title>(.*)</title>\s*<link>(.*)</link>\s*<description>.*</description>\s*<author>.*</author>\s*(<category>.*</category>)?\s*<guid.*</guid>\s*<pubDate>(.*)</pubDate>\s*</item>')
 
-            master = tk.Tk()
-            master.geometry("300x300")
-            master.maxsize(4000000000, 600)
+            root = tk.Tk()
+            scrollbar = tk.Scrollbar(root, orient="vertical")
+            lb = tk.Listbox(root, width=190, height=20, yscrollcommand=scrollbar.set)
+            scrollbar.config(command=lb.yview)
 
-            scrollbar = Scrollbar(master)
-            scrollbar.pack(side=RIGHT, fill=Y)
+            scrollbar.pack(side="right", fill="y")
+            lb.pack(side="left", fill="both", expand=True)
+            for i in range(len(captura_impresa)):
+                for z in range(len(captura_impresa[i])):
+                    lb.insert("end", captura_impresa[i][z])
+            root.mainloop()
 
-            listbox = Listbox(master, yscrollcommand=scrollbar.set)
-            for i in range(len(busca.imprime(captura_filtrada))):
-                listbox.insert(END, str(busca.imprime(captura_filtrada)[i]))
-            listbox.pack(side=LEFT, fill=BOTH)
+        def almacena():
+            busca = Busca()
+            captura_impresa = busca.imprime_url_filtro('http://www.us.es/rss/feed/portada', r'<item>\s*<title>(.*)</title>\s*<link>(.*)</link>\s*<description>.*</description>\s*<author>.*</author>\s*(<category>.*</category>)?\s*<guid.*</guid>\s*<pubDate>(.*)</pubDate>\s*</item>')
 
-            scrollbar.config(command=listbox.yview())
-#            messagebox.showinfo("List", busca.imprime(captura_filtrada))
+            con = None
+            try:
+                con = lite.connect('test.db')
+                with con:
+                    cur = con.cursor()
+                    cur.execute("DROP TABLE IF EXISTS noticias")
+                    cur.execute("CREATE TABLE noticias(id INT, title TEXT, link TEXT, date TEXT)")
+                    for i in range(len(captura_impresa)):
+                        cur.execute("INSERT INTO noticias VALUES(1,'Audi','52642','kmk')")
 
+                    cur = con.cursor()
+                    cur.execute("SELECT * FROM noticias")
+                    rows = cur.fetchall()
+                    for row in rows:
+                        print(row)
+            except lite.Error as e:
+                print("Error {}:".format(e.args[0]))
+                sys.exit(1)
+            finally:
+                if con:
+                    con.close()
 
+        top = tk.Tk()
 
+        button_almacenar = Button(top, text="Almacenar", command=almacena)
+        button_almacenar.pack(side=tk.LEFT)
 
+        button_list = Button(top, text="Listar", command=list)
+        button_list.pack(side=tk.LEFT)
 
-
-
-        B = Button(top, text ="Almacenar")
-        B.pack(side=tk.LEFT)
-
-        C = Button(top, text ="Listar", command = list)
-        C.pack(side=tk.LEFT)
-
-        D = Button(top, text ="Buscar")
-        D.pack(side=tk.LEFT)
+        button_buscar = Button(top, text="Buscar")
+        button_buscar.pack(side=tk.LEFT)
 
         top.mainloop()
 
