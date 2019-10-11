89f60843fa7f5535c802cbf6e36d05655c20424d
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-10-11 11:11:28 +0200

    Terminado ejercicio 2

--- Ejercicios Phyton/Boletin 2/ejercicios2.py
+++ Ejercicios Phyton/Boletin 2/ejercicios2.py
@@ -25,9 +25,15 @@ def imprime(captura_filtrada):
 
 if __name__ == "__main__":
     try:
+        aux = []
+        mes = input('Introduzca un mes de búsqueda(00, 01, ...):\n')
         tag = capturar('http://www.us.es/rss/feed/portada')
         captura_filtrada = filtra(r'<item>\s*<title>(.*)</title>\s*<link>(.*)</link>\s*<description>.*</description>\s*<author>.*</author>\s*(<category>.*</category>)?\s*<guid.*</guid>\s*<pubDate>(.*)</pubDate>\s*</item>', tag)
-        imprime(captura_filtrada)
+        for i in range(len(captura_filtrada)):
+            fecha = dateutil.parser.parse(captura_filtrada[i][3]).strftime("%d/%m/%Y")
+            if mes in fecha:
+                aux.append(captura_filtrada[i])
+        if len(aux) > 0: imprime(aux)
     except HTTPError as e:
         print("Ocurrió un error")
         print(e.code)
