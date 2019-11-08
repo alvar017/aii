631b0354a047526e99deb29f4910369342cc1d1d
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-11-08 21:30:00 +0100

    P3 First Version

---
+++ Prácticas/P3/p3.py
@@ -0,0 +1,113 @@
+#encoding:utf-8
+from tkinter import *
+from tkinter import messagebox
+from bs4 import BeautifulSoup
+from urllib import request
+import os
+import dateutil.parser
+from whoosh.index import create_in,open_dir
+from whoosh.fields import Schema, TEXT, KEYWORD
+from whoosh.qparser import QueryParser
+
+
+def find_url(url):
+    res = []
+    f = request.urlopen(url)
+    page = f.read().decode(f.headers.get_content_charset())
+    f.close()
+
+    soup = BeautifulSoup(page, 'html.parser')
+    threads = soup.findAll("li", {"class": "threadbit"})
+    for i in range(len(threads)):
+        title = threads[i].find("a", {"class": "title"}).get('title')
+        link = 'https://foros.derecho.com/' + threads[i].find("a", {"class": "title"}).get('href')
+        author = threads[i].find("a", {"class": "username understate"}).next
+        date = threads[i].find("a", {"class": "username understate"}).nextSibling.string[2:]
+        date_parse = dateutil.parser.parse(date).strftime("%d/%m/%Y")
+        answers_and_visits = threads[i].findAll("li")
+        answers = answers_and_visits[0].text[-1:]
+        visits = answers_and_visits[1].text[-1:]
+
+        aux = [title, link, author, date_parse, answers, visits]
+        res.append(aux)
+    return res
+
+
+def apartado_a(dirindex):
+    if not os.path.exists(dirindex):
+        os.mkdir(dirindex)
+
+    ix = create_in(dirindex, schema=get_schema_thread())
+    writer = ix.writer()
+    i=0
+    url = "https://foros.derecho.com/foro/34-Derecho-Inmobiliario"
+    answers = find_url(url)
+    for answer in answers:
+        if not os.path.isdir(url+answer[0]):
+            add_doc(writer, answer)
+            i+=1
+    messagebox.showinfo("Fin de indexado", "Se han indexado "+str(i)+ " temas")
+            
+    writer.commit()
+
+    
+def apartado_b(dirindex):
+    def mostrar_lista(event):
+        lb.delete(0,END)   #borra toda la lista
+        ix=open_dir(dirindex)      
+        with ix.searcher() as searcher:
+            query = QueryParser("title", ix.schema).parse(str(en.get()))
+            results = searcher.search(query)
+            for r in results:
+                lb.insert(END,r['title'])
+                lb.insert(END,r['author'])
+                lb.insert(END, r['date'])
+                lb.insert(END,'')
+    
+    v = Toplevel()
+    v.title("Busqueda por rttes")
+    f =Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca el correo del rtte:")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, yscrollcommand=sc.set)
+    lb.pack(side=BOTTOM, fill = BOTH)
+    sc.config(command = lb.yview)    
+
+
+def get_schema_thread():
+    return Schema(title=KEYWORD(stored=True), link=TEXT(stored=True), author=TEXT(stored=True), date=TEXT(stored=True), answers=TEXT(stored=True), visits=TEXT(stored=True))
+
+
+def get_schema_answers():
+    return Schema(link=TEXT(stored=True), date=TEXT(stored=True), response=TEXT(stored=True), author=KEYWORD(stored=True))
+
+
+def add_doc(writer, answer):
+    title = answer[0].strip()
+    link = answer[1].strip()
+    author = answer[2].strip()
+    date = answer[3].strip()
+    answers = answer[4].strip()
+    visits = answer[5].strip()
+    
+    writer.add_document(title=title, link=link, author=author, date=date, answers=answers, visits=visits)
+
+    
+def ventana_principal():
+    dirindex="C:/Users/Alvaro/OneDrive - UNIVERSIDAD DE SEVILLA/AII/aii/Ejercicios Phyton/Boletin 6/Index"
+    top = Tk()
+    indexar = Button(top, text="Indexar", command = lambda: apartado_a(dirindex))
+    indexar.pack(side = TOP)
+    Buscar = Button(top, text="Buscar por Rtte", command = lambda: apartado_b(dirindex))
+    Buscar.pack(side = TOP)
+    top.mainloop()
+
+
+if __name__ == '__main__':
+    ventana_principal()
