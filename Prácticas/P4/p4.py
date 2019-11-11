8d7a86cb28e8c01766cf51fa8cd7081554f38433
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-11-11 20:07:23 +0100

    Indexado de paginas finalizado

--- Prácticas/P4/p4.py
+++ Prácticas/P4/p4.py
@@ -1,118 +1,109 @@
 #encoding:utf-8
+import locale
 from tkinter import *
 from tkinter import messagebox
+from bs4 import BeautifulSoup
+from urllib import request
 import os
+from datetime import datetime
 from whoosh.index import create_in,open_dir
 from whoosh.fields import Schema, TEXT, KEYWORD, DATETIME
-from whoosh.qparser import MultifieldParser
-from datetime import datetime
 from whoosh.qparser import QueryParser
 
 
-def get_schema_correo():
-    return Schema(remitente=TEXT(stored=True), destinatarios=KEYWORD(stored=True), fecha=DATETIME(stored=True),
-                  asunto=TEXT(stored=True), contenido=TEXT(stored=True))
+def find_url(url):
+    res = []
+    f = request.urlopen(url)
+    page = f.read().decode(f.headers.get_content_charset())
+    f.close()
+
+    soup = BeautifulSoup(page, 'html.parser')
+    threads = soup.find("div", {"class": "col-left"}).findAll("div", {"class": "news-card"})
+    for t in threads:
+        title = t.find("a", {"class": "meta-title-link"}).next
+        link_aux = t.find("figure", {"class": "thumbnail"}).find("img", {"class": "thumbnail-img"})
+        if link_aux.get("data-src") is None:
+            link = t.find("figure", {"class": "thumbnail"}).find("img", {"class": "thumbnail-img"}).get("src")
+        else:
+            link = t.find("figure", {"class": "thumbnail"}).find("img", {"class": "thumbnail-img"}).get("data-src")
+        categoria = t.find("div", {"class": "meta-category"}).next
+        descripcion = t.find("div", {"class": "meta-body"})
+        if descripcion != None:
+            descripcion = descripcion.text
+        else:
+            descripcion = ""
+        locale.setlocale(locale.LC_ALL, 'esp_esp')
+        date_aux = str(t.find("div", {"class": "meta-date"}).next).split(",")[1].strip()
+        date = datetime.strptime(date_aux, '%d de %B de %Y')
+        aux = [categoria, title, link, descripcion, date]
+        res.append(aux)
+    return res
 
 
-def get_schema_agenda():
-    return Schema(email=TEXT(stored=True), nombre=TEXT(stored=True))
+def find_url_aux(url):
+    res = []
+    pages = [1, 2, 3]
+    for i in pages:
+        res.extend(find_url(url + "?page=" + str(i)))
+    return res
 
 
-def add_doc_mail(dir_mails, dir_index):
-    ix = create_in(dir_index, schema=get_schema_correo())
+def get_schema_thread():
+    return Schema(category=TEXT(stored=True), title=TEXT(stored=True), link=TEXT(stored=True), description=TEXT(stored=True), date=DATETIME(stored=True))
+
+
+def add_doc(writer, answer):
+    category = answer[0].strip()
+    title = answer[1].strip()
+    link = answer[2].strip()
+    description = answer[3].strip()
+    date = answer[4]
+
+    writer.add_document(category=category, title=title, link=link, description=description, date=date)
+
+
+def apartado_a(dirindex):
+    if not os.path.exists(dirindex):
+        os.mkdir(dirindex)
+
+    ix = create_in(dirindex, schema=get_schema_thread())
     writer = ix.writer()
     i = 0
-    for docname in os.listdir(dir_mails):
-        if not os.path.isdir(dir_mails+docname):
-            fileobj = open(dir_mails + '/' + docname, "r")
-            rte = fileobj.readline().strip()
-            dtos = fileobj.readline().strip()
-            f = fileobj.readline().strip()
-            fcha = datetime.strptime(f, '%Y%m%d')
-            ast = fileobj.readline().strip()
-            ctdo = fileobj.read()
-            fileobj.close()
-            writer.add_document(remitente=rte, destinatarios=dtos, fecha=fcha, asunto=ast, contenido=ctdo)
-            i+=1
+    url = "http://www.sensacine.com/noticias/"
+    answers = find_url_aux(url)
+    for answer in answers:
+        if not os.path.isdir(url+answer[0]):
+            add_doc(writer, answer)
+            i += 1
+    messagebox.showinfo("Fin de indexado", "Se han indexado " + str(i) + " temas")
+            
     writer.commit()
-    return i
 
 
-def add_doc_contact(dir_contacts, dir_index):
-    iy = create_in(dir_index, schema=get_schema_agenda())
-    writer = iy.writer()
-    z = 0
-    for docname in os.listdir(dir_contacts):
-        if not os.path.isdir(dir_contacts+docname):
-            with open(dir_contacts + '/' + docname, "r") as f:
-                lineas = f.readlines()
-                i = 0
-                while i < (len(lineas)):
-                    ml = lineas[i].strip()
-                    nme = lineas[i + 1].strip()
-                    i = i + 2
-                    writer.add_document(email=ml, nombre=nme)
-                    z = z + 1
-    writer.commit()
-    return z
-
-
-def index(dir_mails, dir_contacts, dir_index):
-    if not os.path.exists(dir_mails):
-        print ("Error: no existe el directorio de correos: " + dir_mails)
-        if not os.path.exists(dir_contacts):
-            print("Error: no existe el directorio de contactos: " + dir_contacts)
+def apartado_b(dirindex, search):
+    def mostrar_lista(event):
+        lb.delete(0,END)   #borra toda la lista
+        ix=open_dir(dirindex)      
+        with ix.searcher() as searcher:
+            query = QueryParser(search, ix.schema).parse(str(en.get()))
+            results = searcher.search(query)
+            for r in results:
+                lb.insert(END,r['title'])
+                lb.insert(END,r['author'])
+                lb.insert(END, r['date'])
+                lb.insert(END,'')
+    
+    v = Toplevel()
+    if (search == 'title'):
+        ref = 'Introduzca el título a buscar'
+    elif (search == 'author'):
+        ref = 'Introduzca el nombre del autor'
     else:
-        if not os.path.exists(dir_index + str(1)):
-            os.mkdir(dir_index + str(1))
-        if not os.path.exists(dir_index + str(2)):
-            os.mkdir(dir_index + str(2))
-
-    i = add_doc_mail(dir_mails, dir_index + str(1))
-    z = add_doc_contact(dir_contacts, dir_index + str(2))
-
-    messagebox.showinfo("Fin de indexado", "Se han indexado "+str(i)+ " correos y " + str(z) + " contactos")
-
-
-def search(types, text, dir_index, to_save):
-    res = []
-    ix = open_dir(dir_index)
-    with ix.searcher() as searcher:
-        if len(types) == 1 and types[0] == 'fecha':
-            myquery = '{' + text + 'TO]'
-            q = QueryParser("fecha", ix.schema).parse(myquery)
-        else:
-            qp = MultifieldParser(types, ix.schema)
-            q = qp.parse(text)
-        results = searcher.search(q)
-        for r in results:
-            aux = []
-            for element in to_save:
-                aux.append(r[element])
-            res.append(aux)
-        return res
-
-    
-def apartado_a(dir_index):
-    def mostrar_lista(event):
-        lb.delete(0,END)   #borra toda la lista
-        busqueda1 = search(["asunto", "contenido"], str(en.get()), dir_index + str(1), ["destinatarios", "asunto"])
-        for elemento in busqueda1:
-            correos = elemento[0].split(" ")
-            for correo in correos:
-                nombres = search(["email"], str(correo), dir_index + str(2), ["nombre"])
-                i = 1
-                for nombre in nombres:
-                    lb.insert(END, "Nombre (" + str(i) + "): " + str(nombre[0]))
-                    i = i + 1
-                lb.insert(END, "Asunto del correo: " + elemento[1])
-                lb.insert(END, " ")
-
-    v = Toplevel()
-    v.title("Busqueda por asunto y remitente")
+        ref = 'Búsqueda'
+    v.title('Búsqueda')
     f =Frame(v)
     f.pack(side=TOP)
-    l = Label(f, text=" Introduzca el texto a buscar:")
+    l = Label(f, text=ref)
     l.pack(side=LEFT)
     en = Entry(f)
     en.bind("<Return>", mostrar_lista)
@@ -121,50 +112,31 @@ def apartado_a(dir_index):
     sc.pack(side=RIGHT, fill=Y)
     lb = Listbox(v, yscrollcommand=sc.set)
     lb.pack(side=BOTTOM, fill = BOTH)
-    sc.config(command = lb.yview)
+    sc.config(command = lb.yview)    
 
 
-def apartado_b(dir_index):
-    def mostrar_lista(event):
-        lb.delete(0,END)   #borra toda la lista
-        busqueda1 = search(["fecha"], str(en.get()), dir_index + str(1), ["remitente", "destinatarios", "asunto"])
-        for elemento in busqueda1:
-            correos = elemento[0].split(" ")
-            for correo in correos:
-                nombres = search(["email"], str(correo), dir_index + str(2), ["nombre"])
-                i = 1
-                for nombre in nombres:
-                    lb.insert(END, "Nombre (" + str(i) + "): " + str(nombre[0]))
-                    i = i + 1
-                lb.insert(END, "remitente")
-        lb.insert(END, " ")
-
-    v = Toplevel()
-    v.title("Busqueda por asunto y remitente")
-    f =Frame(v)
-    f.pack(side=TOP)
-    l = Label(f, text=" Introduzca el texto a buscar:")
-    l.pack(side=LEFT)
-    en = Entry(f)
-    en.bind("<Return>", mostrar_lista)
-    en.pack(side=LEFT)
-    sc = Scrollbar(v)
-    sc.pack(side=RIGHT, fill=Y)
-    lb = Listbox(v, yscrollcommand=sc.set)
-    lb.pack(side=BOTTOM, fill = BOTH)
-    sc.config(command = lb.yview)
-    
 def ventana_principal():
-    dir_mails = "C:/Users/Alvaro/OneDrive - UNIVERSIDAD DE SEVILLA/AII/aii/Ejercicios Phyton/Boletin 7/Correos"
-    dir_contacts = "C:/Users/Alvaro/OneDrive - UNIVERSIDAD DE SEVILLA/AII/aii/Ejercicios Phyton/Boletin 7/Agenda"
-    dir_index = "C:/Users/Alvaro/OneDrive - UNIVERSIDAD DE SEVILLA/AII/aii/Ejercicios Phyton/Boletin 7/Index"
+    dirindex="Index"
     top = Tk()
-    indexar = Button(top, text="Indexar", command = lambda: index(dir_mails, dir_contacts, dir_index))
-    indexar.pack(side = TOP)
-    Buscar1= Button(top, text="Búsqueda por asunto o contenido", command = lambda: apartado_a(dir_index))
-    Buscar1.pack(side = TOP)
-    Buscar2 = Button(top, text="Buscar correos posteriores a una fecha (YYYYMMDD)", command = lambda: apartado_b(dir_index))
-    Buscar2.pack(side = TOP)
+    top.geometry("198x0")
+
+    def close_window():
+        top.destroy()
+
+    menubar = Menu(top)
+    top.config(menu=menubar)
+
+    data_menu = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Inicio", menu=data_menu)
+    data_menu.add_command(label="Indexar", command=lambda: apartado_a(dirindex))
+    data_menu.add_separator()
+    data_menu.add_command(label="Salir", command=close_window)
+
+    data_menu = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Buscar", menu=data_menu)
+    data_menu.add_command(label="Título", command=lambda: apartado_b(dirindex, 'title'))
+    data_menu.add_command(label="Autor", command=lambda: apartado_b(dirindex, 'author'))
+
     top.mainloop()
 
 
